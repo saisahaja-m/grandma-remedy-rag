9006a1bc60300245d31551354f739a7dea0833e5
sahaja <sahaja.mylapally@ibhubs.co.in>
2025-04-10 18:04:27 +0530

    fixed a bug

--- data_retrieving.py
+++ data_retrieving.py
@@ -1,5 +1,5 @@
 
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.retrievers import BM25Retriever, EnsembleRetriever
 import google.generativeai as genai
